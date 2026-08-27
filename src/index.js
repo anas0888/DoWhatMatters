56872838b9b3c5b84b483f6d809c02d15c5a287d
Muhammad Anas <daggaanas0888@gmail.com>
2026-08-27 13:32:29 +0530

    import task and inbox and make then functional

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./style.css";
+import "./addTask.js"
+import "./inbox.js";
+import { addTask } from "./addTask.js";
+import { inbox } from "./inbox.js";
 
-const myArray = ["apple", "banana","grapes"];
 
-console.log(myArray);
+
+const addTaskBtn = document.querySelector('.addTaskBtn');
+addTaskBtn.addEventListener('click',() => {
+    const newTask = addTask();
+const inboxContainer = inbox();
+
+inboxContainer.append(newTask);
+})
