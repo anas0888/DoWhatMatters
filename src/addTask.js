4194cbf0c504c81097083179b3be48734e7f826a
Muhammad Anas <daggaanas0888@gmail.com>
2026-08-29 15:35:20 +0530

    add dialog box for tasks

--- src/addTask.js
+++ src/addTask.js
@@ -1,6 +1,17 @@
 const addTask = () => {
 const task = document.createElement('div');
-task.innerHTML = "hello this is a div";
+task.innerHTML = `
+<dialog id = "taskDialog">
+    <p>Enter your task</p>
+    <input type="text" id = "userInput" placeholder="Task of the day">
+    </input>
+    <div class = "dialogBtn">
+        <button type="button" id = "cancelBtn">Cancel</button>
+        <button type="submit" id = "submitBtn">Submit</button>
+    </div>
+</dialog>`
+
+
     return task;
 }
 export{addTask}
