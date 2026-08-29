7142e90498e35af052a5162ad9cc05a546bd12d6
Muhammad Anas <daggaanas0888@gmail.com>
2026-08-29 15:35:46 +0530

    dialog box

--- src/index.js
+++ src/index.js
@@ -7,9 +7,33 @@ import { inbox } from "./inbox.js";
 
 
 const addTaskBtn = document.querySelector('.addTaskBtn');
-addTaskBtn.addEventListener('click',() => {
-    const newTask = addTask();
-const inboxContainer = inbox();
 
-inboxContainer.append(newTask);
+
+const userInput = document.getElementById('userInput');
+
+addTaskBtn.addEventListener('click', () => {
+    const newTask = addTask();
+    const inboxContainer = inbox(); 
+    inboxContainer.append(newTask);
+
+    const dialog = document.getElementById('taskDialog');
+    const cancelBtn = document.getElementById('cancelBtn');
+
+    if(dialog && cancelBtn){
+        dialog.showModal();
+
+        cancelBtn.onclick = () => {
+            dialog.close();
+        }
+        }
+        else{
+            console.log("error in the dialog box")
+        }
+
+
+
 })
+
+cancelBtn.addEventListener('click', () => {
+    dialog.close();
+});
